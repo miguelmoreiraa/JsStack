3c11494e85556773058168f9da61d114c358b76c
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2024-07-23 21:46:43 -0300

    First Commit in project

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+const { printName, lastName } = require("./printName");
+printName(`Miguel ${lastName}`);
+
+// const os = require("os");
+// console.log(`Operating System: ${os.type()} ${os.release()}`);
